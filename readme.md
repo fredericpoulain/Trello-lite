f70222dcdce52cd8112ff075a19e2d23409ef5be
Frédéric Poulain <fredpoulain0210@gmail.com>
2024-04-07 09:05:54 +0200

    add readme

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Trello "Lite"
+
+Trello "Lite" est une version simplifié de Trello, développé avec Symfony, PHP, React, TailwindCSS et Twig
+
+## **Objectif du Projet** : 
+
+Gérer des tâches et des projets de manière efficace et intuitive. Ce projet vise à offrir une expérience utilisateur améliorée, en se concentrant sur la facilité d'utilisation et la clarté des fonctionnalités.
+
+## **Technologies Utilisées :**
+
+* **Backend** : Symfony (Framework PHP)
+* **Base de Données** : Doctrine ORM et MySQL
+* **Frontend** : ReactJS (pour la création de composants dynamiques) et TailwindCSS (pour le style et la mise en page)
+* **Template Engine** : Twig (pour la génération de vues côté serveur)
+
+## **Fonctionnalités Principales :**
+
+* **Système d'Inscription et d'Authentification** : Permet aux utilisateurs de s'inscrire et de se connecter à leur compte pour accéder à leurs espaces de travail et listes.
+* **Gestion des Espaces de Travail** : Possibilité de créer, supprimer ou modifier des "espaces de travail", permettant aux utilisateurs de regrouper leurs projets et tâches.
+* **Gestion des Listes** : Permet aux utilisateurs de créer, supprimer ou modifier des "listes" pour organiser leurs tâches dans les espaces de travail.
+* **Drag and Drop** : Utilisation de ReactJS et de la librairie "@hello-pangea/dnd" pour permettre aux utilisateurs de réorganiser facilement les tâches et les listes à l'aide de la fonctionnalité de glisser-déposer.
+
+"Trello Lite" est un projet qui met l'accent sur la simplicité et l'efficacité, tout en conservant les fonctionnalités essentielles de gestion de projets.
+Ce projet démontre ma capacité à créer des applications web interactives et performantes, tout en utilisant les technologies modernes pour répondre aux besoins des utilisateurs.
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,9 +6,12 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
@@ -16,7 +19,14 @@ use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 class RegistrationController extends AbstractController
 {
     #[Route('/enregistrement', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request                     $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        UserAuthenticatorInterface  $userAuthenticator,
+        UserAuthenticator           $authenticator,
+        EntityManagerInterface      $entityManager,
+        MailerInterface             $mailer
+    ): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_home');
@@ -33,7 +43,25 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            $userEmail = $form->get('email')->getData();
+            $userName = $form->get('userName')->getData();
+            $email = (new TemplatedEmail())
+                ->from('inscription@monsite.fr')
+                ->to($userEmail)
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Inscription')
+                ->htmlTemplate('emails/register.html.twig')
+                ->locale('fr')
 
+                // pass variables (name => value) to the template
+                ->context([
+                    'username' => $userName,
+                ]);
+
+            $mailer->send($email);
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
